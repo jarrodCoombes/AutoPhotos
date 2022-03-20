b46f2e0ed534351357392810a7a7a9c721cb7945
Jarrod <58901318+jarrodCoombes@users.noreply.github.com>
2022-03-20 04:48:57 -0700

    Update autophoto.sh

--- autophoto.sh
+++ autophoto.sh
@@ -3,29 +3,30 @@
 version=0.1.15
 
 if [ "$1" = "-v" ]; then
-	printf "\nAutoPhoto Version $version\n"
-	exit
+	printf "AutoPhoto Version $version\n"
+	exit 0
 fi
 
-cwd=$(pwd)
-target=resized
-output_size=1920x1080
+target=resized        #Name of the sub directory for the resized images.
+output_size=1920x1080 #Output resolution for the resized images.
 
 #Check to make sure we're being run as root
 if ! [ $(id -u) = 0 ]
   then printf "\nPlease run as root\n\n"
-  exit
+  exit 1
 fi
 
 
 inotifywait -mr --timefmt '%m/%d/%y %H:%M' --format '%T %w%f' -e close_write --exclude '/resized(/.*)?$' /shares/Photos |
     grep --line-buffered -Ei '/[^/]*\.(jpg|jpeg)$' |
 while read -r date time changed_abs; do
+
     [ -d "$changed_abs" ] && continue # a directory looking like a picture filename was written to: skip this event
-    dir="${changed_abs%/*}/"
-    file="${changed_abs##*/}"
-    small_dir="${dir}${target}/"
-    output_file="${small_dir}"1080-"${file}"
+
+    dir="${changed_abs%/*}/"  					#The full path of the folder the file is in.
+    file="${changed_abs##*/}" 					#The file name of the file
+    small_dir="${dir}${target}/"				#The targer directory in which the resized image will be put in.
+    output_file="${small_dir}"1080-"${file}"	#The filename of the resized file.
 	
     printf "\nFile $changed_abs was changed or created\n  dir=$dir \n  file=$file \n  small_dir=$small_dir \n"
 
@@ -42,8 +43,7 @@ while read -r date time changed_abs; do
 			chmod 777 "$small_dir"
 		fi
 
-		printf "\nResizing file into the $small_dir folder\n"
-		# Code to resize image goes here
+		# printf "\nResizing file into the $small_dir folder\n"
 		printf "\n  Converting: $changed_abs \n  Output Size:$output_size \n  Output File: $output_file\n" 
 		convert "$changed_abs" -resize "$output_size" "$output_file"
 		printf "\nDone\n\n"
