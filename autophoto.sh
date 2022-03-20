8c6c1169b935f96cb00075156df320e0a20d8ce6
Jarrod <58901318+jarrodCoombes@users.noreply.github.com>
2022-03-19 22:48:21 -0700

    Update autophoto.sh

--- autophoto.sh
+++ autophoto.sh
@@ -5,10 +5,15 @@
 cwd=$(pwd)
 
 inotifywait -mr --timefmt '%m/%d/%y %H:%M' --format '%T %w %f' -e close_write /shares/Photos |
-while read -r date time dir file; do
+while read -r dir file; do
        changed_abs=${dir}${file}
        changed_rel=${changed_abs#"$cwd"/}
 
-       # rsync --progress --relative -vrae 'ssh -p 22' "$changed_rel" usernam@example.com:/backup/root/dir && \
-       printf "\nAt ${time} on ${date}, file $changed_abs was changed or created ( dir =$dir file=$file ) \n"
+    if [[ "$file" =~ .*jpg$ ]]; then # Does the file end with .jpg?
+       printf "\nFile $changed_abs was changed or created ( dir =$dir file=$file ) \n"
+    else
+	   printf "\nFile $changed_abs was changed or created ( dir =$dir file=$file ) but was not a jpg \n"
+	fi
+
+       
 done
