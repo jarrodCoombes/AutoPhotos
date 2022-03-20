62b167486cab58eec16a32a65944efa001566dfb
Jarrod <58901318+jarrodCoombes@users.noreply.github.com>
2022-03-19 23:17:20 -0700

    Update autophoto.sh

--- autophoto.sh
+++ autophoto.sh
@@ -4,7 +4,7 @@
 
 cwd=$(pwd)
 
-inotifywait -mr --timefmt '%m/%d/%y %H:%M' --format '%T %w %f' -e close_write /shares/Photos --exclude "\.jpg" |
+inotifywait -mr --timefmt '%m/%d/%y %H:%M' --format '%T %w %f' -e close_write /shares/Photos --includei "\.jpg" |
 while read -r date time dir file; do
        changed_abs=${dir}${file}
        changed_rel=${changed_abs#"$cwd"/}
