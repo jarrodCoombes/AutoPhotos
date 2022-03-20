e9d1e0b50054caed5ff00d5cbc418399a945cae0
Jarrod <58901318+jarrodCoombes@users.noreply.github.com>
2022-03-19 23:43:13 -0700

    Update autophoto.sh

--- autophoto.sh
+++ autophoto.sh
@@ -14,22 +14,22 @@ if ! [ $(id -u) = 0 ]
 fi
 
 
-inotifywait -mr --timefmt '%m/%d/%y %H:%M' --format '%T %w %f' -e close_write /shares/Photos --includei "\.jpg" |
+inotifywait -mr --timefmt '%m/%d/%y %H:%M' --format '%T %w %f' -e close_write /shares/Photos --includei "\.jpg" exclude "$target\*"|
 while read -r date time dir file; do
-       changed_abs=${dir}${file}
-       small_dir=${dir}${target}
-	   #changed_rel=${changed_abs#"$cwd"/}
-
-       printf "\nFile $changed_abs was changed or created\n  dir=$dir \n  file=$file \n  small_dir=$small_dir \n"
-	   
+    changed_abs=${dir}${file}
+    small_dir=${dir}${target}
 	
- 
+    printf "\nFile $changed_abs was changed or created\n  dir=$dir \n  file=$file \n  small_dir=$small_dir \n"
+      
 	if [ -d "$small_dir" -a ! -h "$small_dir" ]
 	then
 		echo "$small_dir found, nothing to do."
 	else
 		echo "Creating $small_dir"
 		mkdir $small_dir
-fi
-       
+	fi
+
+	
+
+	
 done
