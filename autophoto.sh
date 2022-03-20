7b86f09880a9c78030a29fb7425dfb77143e4817
Jarrod <58901318+jarrodCoombes@users.noreply.github.com>
2022-03-19 23:27:04 -0700

    Update autophoto.sh

--- autophoto.sh
+++ autophoto.sh
@@ -9,8 +9,10 @@ cwd=$(pwd)
 inotifywait -mr --timefmt '%m/%d/%y %H:%M' --format '%T %w %f' -e close_write /shares/Photos --includei "\.jpg" |
 while read -r date time dir file; do
        changed_abs=${dir}${file}
-       changed_rel=${changed_abs#"$cwd"/}
+       small_dir=${dir}/small
+	   #changed_rel=${changed_abs#"$cwd"/}
 
-       printf "\nFile $changed_abs was changed or created ( dir =$dir file=$file ) \n"
+       printf "\nFile $changed_abs was changed or created\n  dir=$dir \n  file=$file \n  small_dir=$small_dir \n"
+	   mkdir $
        
 done
