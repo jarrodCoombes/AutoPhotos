13a42fc9992a76fe2d0bd5e0ca354937c5ca1fca
Jarrod <58901318+jarrodCoombes@users.noreply.github.com>
2022-03-20 01:26:00 -0700

    Update autophoto.sh

--- autophoto.sh
+++ autophoto.sh
@@ -20,20 +20,28 @@ while read -r date time dir file; do
     small_dir=${dir}${target}/
 	
     printf "\nFile $changed_abs was changed or created\n  dir=$dir \n  file=$file \n  small_dir=$small_dir \n"
-      
-	if [ -d "$small_dir" -a ! -h "$small_dir" ]
-	then
-		echo "$small_dir found, nothing to do."
-	else
-		echo "Creating $small_dir"
-		mkdir $small_dir
-		chmod 777 $small_dir
-	fi
-	
-	if [ "$dir" = "$small_dir" ]; then
+
+    # Check to make sure the file is not allready in $small_dir    
+	if [ "${dir##*/}" = "$target" ]; then
 		printf "\nFile is in the $small_dir folder, nothing to do\n"
 	else
 		printf "\nResizing file into the $small_dir folder\n"
+		
+		#Check to see if $small_dir exists or not
+		if [ -d "$small_dir" -a ! -h "$small_dir" ]; then
+			echo "$small_dir found, nothing to do."
+		else
+			echo "Creating $small_dir"
+			mkdir $small_dir
+			chmod 777 $small_dir
+		fi
+	
+	# Code to resize image goes here
+	
 	fi
+		
+		
+	
+
 		
 done
