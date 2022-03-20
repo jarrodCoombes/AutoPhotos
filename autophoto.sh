5d4e6eb12648bb06d1858e2fae4985e572449461
Jarrod <58901318+jarrodCoombes@users.noreply.github.com>
2022-03-19 22:58:28 -0700

    Update autophoto.sh

--- autophoto.sh
+++ autophoto.sh
@@ -4,16 +4,11 @@
 
 cwd=$(pwd)
 
-inotifywait -mr --timefmt '%m/%d/%y %H:%M' --format '%T %w %f' -e close_write /shares/Photos |
+inotifywait -mr --timefmt '%m/%d/%y %H:%M' --format '%T %w %f' -e close_write /shares/Photos --include "\.jpg" |
 while read -r date time dir file; do
        changed_abs=${dir}${file}
        changed_rel=${changed_abs#"$cwd"/}
 
-    if [[ "$file" =~ .*jpg$ ]]; then # Does the file end with .jpg?
        printf "\nFile $changed_abs was changed or created ( dir =$dir file=$file ) \n"
-    else
-	   printf "\nFile $changed_abs was changed or created ( dir =$dir file=$file ) but was not a jpg \n"
-	fi
-
        
 done
