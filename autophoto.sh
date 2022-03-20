c2cebd4e6671eade0e0ae71f77a376f65f305b0a
Jarrod <58901318+jarrodCoombes@users.noreply.github.com>
2022-03-19 22:36:27 -0700

    Update autophoto.sh

--- autophoto.sh
+++ autophoto.sh
@@ -10,5 +10,5 @@ while read -r date time dir file; do
        changed_rel=${changed_abs#"$cwd"/}
 
        # rsync --progress --relative -vrae 'ssh -p 22' "$changed_rel" usernam@example.com:/backup/root/dir && \
-       echo "At ${time} on ${date}, file $changed_abs was backed up via rsync" # >&2
+       printf "\nAt ${time} on ${date}, file $changed_abs was changed or created ( dir =$dir file=$file ) \n"
 done
